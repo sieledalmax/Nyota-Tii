f262ed2f5a4dbf5cd2e5ec46588800ee73929bce
KichwaMbaya <kiptoosiele@yahoo.com>
2025-12-17 10:25:21 +0300

    Payhero akilinusu evans

--- api/initiate-payment.js
+++ api/initiate-payment.js
@@ -13,8 +13,8 @@ export default async function handler(req, res) {
 
     const PAYHERO_CONFIG = {
       apiUrl: 'https://backend.payhero.co.ke/api/v2/payments',
-      basicAuthToken: 'Basic U3FFY3NZb2FCb09nczlQZWVRVU46dWFsQzV3cFJIR2lVODhBOVFoOW5tV3NlWlI1RTRMc0xocDJLWEh5Yw==',
-      channelId: 4610,
+      basicAuthToken: 'Basic M0YzdVM4dHQ0Q2YxTG1zQjRhc0c6SjdYTmg4S2Vsd1hDNUhiNUNsQzd1VUthazIxWko0N0xpN2hicFdTZg==',
+      channelId: 4428,
       provider: 'm-pesa',
       callbackUrl: 'https://samttech.co.ke/callback'
     };
